ed153a9f9e53421d65de606417a9e14566d8102b
DiegoFernando01 <diegofvictoria2013@gmail.com>
2023-05-30 17:05:35 -0500

    Fix recomendation about acount_id

--- ProyectoConsesionario/urls.py
+++ ProyectoConsesionario/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), #Considerar dejar o quitar
     path('', include('ConcesionariaApp.urls')),
     path('gerente/', include('GerenteApp.urls')),
 
