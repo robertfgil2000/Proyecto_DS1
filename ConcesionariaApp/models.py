c69470da120f7718b664b05486dd3f05535ce527
DiegoFernando01 <diegofvictoria2013@gmail.com>
2023-05-30 17:06:37 -0500

    Fix server recomendation about acount_id

--- ConcesionariaApp/models.py
+++ ConcesionariaApp/models.py
@@ -8,7 +8,7 @@ class Gerente(models.Model):
     last_name = models.CharField(max_length=20)
     #llave foranea con Office que esta en models.py de Gerente.py
     office_gerente = models.ForeignKey(Office, null=True, blank = True, on_delete=models.CASCADE)
-    account_id = models.ForeignKey(Account,null=True, blank=True, unique= True, on_delete=models.CASCADE)
+    account_id = models.OneToOneField(Account,null=True, blank=True, unique= True, on_delete=models.CASCADE)
 
     def __str__(self):
        return 'nombre completo: %s %s email: %s pass: %s' %(self.name, self.last_name, self.account_id.email, self.account_id.password) 
